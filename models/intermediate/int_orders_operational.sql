01b922f20f9a8cdb5909b759714fcc1fd90a9c98
Guillaume Fourrier <gb.fourrier@gmail.com>
2026-04-29 14:56:26 +0000

    Calcul margin purchase cost and operationnal margin

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,7 @@
+SELECT orders_id,
+date_date,
+ROUND(margin+shipping_fee-logcost-ship_cost, 2) AS operational_margin,
+quantity,
+FROM {{ ref('int_orders_margin') }}
+INNER JOIN {{ ref('stg_gz_raw_data__raw_gz_ship') }}
+USING (orders_id)
